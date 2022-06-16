fd07b95917987eb629157746ae9d57265d691b52
Thiago Galvao <thiagosilva128@gmail.com>
2022-06-15 23:01:29 -0300

    added validation when some user disconnect from room

--- src/websocket/websocket.gateway.ts
+++ src/websocket/websocket.gateway.ts
@@ -2,23 +2,40 @@ import { Logger } from "@nestjs/common";
 import { OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Socket, Server } from "socket.io";
 
+interface User {
+    id: string,
+    username: string
+}
+
+interface PayloadProp {
+    created_at: string,
+    message: string,
+    username: string
+}
+
 @WebSocketGateway({ cors: "*" })
 export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
     @WebSocketServer()
     server: Server
 
     private logger: Logger = new Logger('AppGateway')
+    private users: User[] = []
 
     @SubscribeMessage('MessageServer')
-    handleMessage(client: Socket, payload: string) {
+    handleMessage(client: Socket, payload: PayloadProp) {
+
+        const equalUser = (({ username }) =>  username === payload.username)
+        const hasUser = this.users.some(equalUser)
+
+        if(!hasUser) {
+            this.users.push({ id: client.id, username: payload.username })
+        }
+
         this.server.emit('newMessage', payload, client.id)
-        console.log(payload)
     }
 
     afterInit(server: Server) {
         this.logger.log('web socket started')
-
-
     }
 
     handleConnection(client: Socket) {
@@ -27,8 +44,15 @@ export class AppGateway implements OnGatewayInit, OnGatewayConnection, OnGateway
         this.logger.log('new user conneted', client.id)
     }
 
-    handleDisconnect(cliente: Socket) {
+    handleDisconnect(client: Socket) {
+        if(!this.users) {
+            return
+        }
         
-    }
+        const whichUserLeft = this.users.find(({ id }) => id === client.id)
+        const remainingUsers = this.users.filter(({ id }) => id !== whichUserLeft.id)
 
+        this.users = remainingUsers
+        this.server.emit('userDisconnected', whichUserLeft)   
+    }
 }
